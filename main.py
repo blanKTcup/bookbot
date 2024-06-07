c013a08a73f3d777e90dca2873208b41966058c4
blanKTcup <kthightran024@gmail.com>
2024-06-06 19:58:59 -0700

    reorganized getting book text, added getting book word count

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 def main():
-  with open("books/frankenstein.txt") as f:
-    file_contents = f.read()
-    print(file_contents)
+  book_path = "books/frankenstein.txt"
+  book_text = get_text(book_path)
+  word_count = get_num_words(book_text)
+  print(f"There are {word_count} words in this book")
+
+def get_num_words(book_text):
+  word_count = len(book_text.split())
+  return word_count
+
+def get_text(path):
+  with open(path) as f:
+    return f.read()
 
 main()
